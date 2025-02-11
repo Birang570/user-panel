fac51fc3b6925d595fd47ad12f24f7f977a7d681
Birang570 <115399671+Birang570@users.noreply.github.com>
2025-02-11 23:40:28 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
+document.querySelector('.dropdown-btn').addEventListener('click', () => {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+});
+
+window.addEventListener('click', (e) => {
+    if (!e.target.matches('.dropdown-btn')) {
+        document.querySelector('.dropdown-menu').style.display = 'none';
+    }
+});
 

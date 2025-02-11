cebb8365345cf2b5251c096600b58771810b22ac
Birang570 <115399671+Birang570@users.noreply.github.com>
2025-02-11 23:41:46 +0530

    Update db_connect.php

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,1 +1,13 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "store";
 
+    $con = mysqli_connect($host,$user,$password,$db);
+
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>

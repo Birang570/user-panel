60bacb7aec1ba0dd56ada99de9a98b0a42718bf5
Birang570 <115399671+Birang570@users.noreply.github.com>
2025-02-20 00:18:13 +0530

    Add files via upload

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,13 +1,15 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $db = "store";
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "store";
 
-    $con = mysqli_connect($host,$user,$password,$db);
+$con = mysqli_connect($host, $user, $password, $db);
 
-    if (mysqli_connect_errno())
-    {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-?>
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$select = mysqli_query($con, 'SELECT storename FROM store limit 1');
+$row = mysqli_fetch_array($select);
+$storeName = "$row[storename]";
